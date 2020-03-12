6480a12b416c2dbe806badae75f7f403c93f36c3
saliWd <57621907+saliWd@users.noreply.github.com>
2020-03-12 11:35:22 +0100

    using live data

--- html/swimmeter/logging/functions.php
+++ html/swimmeter/logging/functions.php
@@ -20,11 +20,8 @@ use pChart\pColor;
 use pChart\pDraw;
 use pChart\pScatter;
 
-function doGraph(array $rssi, array $xAxis): void {
-  $WIDTH = 800;
-  $HEIGHT = 400;
-  
-  $myPicture = new pDraw($WIDTH, $HEIGHT);
+function doGraph(array $rssi, array $xAxis, string $title, int $width, int $height): void {
+  $myPicture = new pDraw($width, $height);
 
   $myPicture->myData->addPoints($xAxis,"time");
   $myPicture->myData->addPoints($rssi,"rssi");
@@ -39,26 +36,26 @@ function doGraph(array $rssi, array $xAxis): void {
   $myPicture->myData->setScatterSerieProperties(0, ["Description" => "RSSI", "Color" => new pColor(0), "Ticks" => 4]);
   
   /* Draw the background */
-  $myPicture->drawFilledRectangle(0,0,$WIDTH,$HEIGHT,["Color"=>new pColor(170,183,87), "Dash"=>TRUE, "DashColor"=>new pColor(190,203,107)]);
+  $myPicture->drawFilledRectangle(0,0,$width,$height,["Color"=>new pColor(170,183,87), "Dash"=>TRUE, "DashColor"=>new pColor(190,203,107)]);
   
   /* Overlay with a gradient */
-  $myPicture->drawGradientArea(0,0,$WIDTH,$HEIGHT,DIRECTION_VERTICAL,["StartColor"=>new pColor(219,231,139,50),"EndColor"=>new pColor(1,138,68,50)]);
-  $myPicture->drawGradientArea(0,0,$WIDTH,20, DIRECTION_VERTICAL,["StartColor"=>new pColor(0,0,0,80),"EndColor"=>new pColor(50,50,50,80)]);
+  $myPicture->drawGradientArea(0,0,$width,$height,DIRECTION_VERTICAL,["StartColor"=>new pColor(219,231,139,50),"EndColor"=>new pColor(1,138,68,50)]);
+  $myPicture->drawGradientArea(0,0,$width,20, DIRECTION_VERTICAL,["StartColor"=>new pColor(0,0,0,80),"EndColor"=>new pColor(50,50,50,80)]);
   
   // default font
   $myPicture->setFontProperties(["FontName"=>"Cairo-Regular.ttf"]);
   
   $myPicture->setFontProperties(["FontSize"=>10]); // title is bigger
-  $myPicture->drawText(10,15,"RSSI versus Zeit",["Color"=>new pColor(255)]);
+  $myPicture->drawText(10,15,$title.': RSSI versus Zeit',["Color"=>new pColor(255)]);
   $myPicture->setFontProperties(["FontSize"=>7]); // back to normal size
   
-  $myPicture->drawRectangle(0,0,$WIDTH-1,$HEIGHT-1,["Color"=>new pColor(0)]); // Add a border to the picture
-  $myPicture->setGraphArea(50,50,$WIDTH-50,$HEIGHT-50); // Set the graph area
+  $myPicture->drawRectangle(0,0,$width-1,$height-1,["Color"=>new pColor(0)]); // Add a border to the picture
+  $myPicture->setGraphArea(50,50,$width-50,$height-50); // Set the graph area
   $myScatter = new pScatter($myPicture); // Create the Scatter chart object
   $myScatter->drawScatterScale(); // Draw the scale
   $myPicture->setShadow(TRUE,["X"=>1,"Y"=>1,"Color"=>new pColor(0,0,0,10)]); // Turn on shadow computing
   $myScatter->drawScatterLineChart(); // Draw a scatter plot chart
   $myScatter->drawScatterLegend(280,380,["Mode"=>LEGEND_HORIZONTAL,"Style"=>LEGEND_NOBORDER]); // Draw the legend
-  $myPicture->autoOutput('graph.png'); // compression etc. at default values
+  $myPicture->autoOutput($title.'.png'); // compression etc. at default values
 }
 
--- html/swimmeter/logging/logging.php
+++ html/swimmeter/logging/logging.php
@@ -45,31 +45,47 @@ a:hover {
 
   <?php // declare(strict_types=1);
   require_once('functions.php');
+  
+  function preprocLastSeen(string $in): string {
+    return substr($in, 5, 8);    
+  }
+  
   $dbConn = initialize();
-  if ($result = $dbConn->query('SELECT * FROM `swLog` WHERE 1 ORDER BY `lastSeen` LIMIT 1000')) { // newest at bottom
+  if ($result = $dbConn->query('SELECT `lastSeen`, `rssi`, `deviceName` FROM `swLog` WHERE 1 ORDER BY `lastSeen` LIMIT 1000')) { // newest at bottom
     if ($result->num_rows == 0) { // most probably a new user
       echo '<div class="row twelve columns linktext">nothing in DB</div>';
     } else {
-      $counter = 0;
+      $WIDTH = 800;
+      $HEIGHT = 400;
+      $rssi = [];
+      $xAxis = [];
+      // TODO: could be converted into a do_while?
+      
+      $row = $result->fetch_assoc(); // do it once (there is at least one result)      
+      $deviceNameOld = $row['deviceName'];
+      $rssi[]   = $row['rssi'];
+      $xAxisOld = $row['lastSeen'];  // ignore some digits (TBD which ones)
+      $xAxis[]  = $xAxisOld;
+      
       while ($row = $result->fetch_assoc()) {
-        echo '<div class="row">
-                <div class="halbeReihe four columns linktext">'.$row['lastSeen'].'</div>
-                <div class="halbeReihe four columns linktext">'.$row['rssi'].'</div>
-                <div class="halbeReihe four columns linktext">'.$row['deviceName'].'</div>
-              </div>'."\n";        
-      } // while    
+        
+        if ($row['deviceName'] != $deviceNameOld) {
+          // TODO: whenever there is a change in device name, I want a new graph
+        }
+        
+        // need to ignore non-distinct values on lastSeen
+        if ($row['lastSeen'] != $xAxisOld) {
+          $deviceNameOld = $row['deviceName'];
+          $rssi[]   = $row['rssi'];
+          $xAxisOld = $row['lastSeen'];
+          $xAxis[]  = $xAxisOld;
+        } // else just skip
+      } // while
+      doGraph($rssi, $xAxis, $deviceNameOld, $WIDTH, $HEIGHT);
+      echo '<div class="row twelve columns"><img src="'.$deviceNameOld.'.png" width="'.$WIDTH.'" height="'.$HEIGHT.'" alt="rssi vs. time plot"></div>';  
     } // have at least one entry
     $result->close(); // free result set
-  } // query 
-
-  
-  // some static data from db. TODO: use the live data
-  $rssi = [-51,-65,-59,-61,-68,-68,-68,-69,-50];
-  $xAxis =[688,788,791,793,794,795,797,799,801]; // attention: those must be distinct, can't have two with same value
-
-  doGraph($rssi, $xAxis);
-
-  echo '<div class="row twelve columns"><img src="graph.png" width="800" height="400" alt="rssi vs. time plot"></div>';  
+  } // query   
   
   ?>  
   </div>
