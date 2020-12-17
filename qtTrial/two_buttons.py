c74448e6ba6e4cb4a4d32c327cdcba4d186e6960
saliWd <57621907+saliWd@users.noreply.github.com>
2020-12-17 14:59:53 +0100

    reformatting according to pylint

--- qtTrial/two_buttons.py
+++ qtTrial/two_buttons.py
@@ -1,38 +1,43 @@
+" A gui for a music player using QT on python"
 import sys
 from PySide6.QtWidgets import (QPushButton, QApplication, QVBoxLayout, QDialog)
 
 class Form(QDialog):
-
+    "defines the buttons and the layouts and stuff"
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
         self.setWindowTitle("Player Control")
 
         # Create two buttons
-        self.buttonNext = QPushButton("Next song")
-        self.buttonPrev = QPushButton("Previous song")        
+        self.button_next = QPushButton("Next song")
+        self.button_prev = QPushButton("Previous song")
 
         # Create layout and add widgets
         layout = QVBoxLayout()
-        layout.addWidget(self.buttonNext)
-        layout.addWidget(self.buttonPrev)
+        layout.addWidget(self.button_next)
+        layout.addWidget(self.button_prev)
 
         # Set dialog layout
         self.setLayout(layout)
 
         # Add button connection
-        self.buttonNext.clicked.connect(self.cmdGotoNext)
-        self.buttonPrev.clicked.connect(self.cmdGotoPrev)
+        self.button_next.clicked.connect(self.cmd_goto_next)
+        self.button_prev.clicked.connect(self.cmd_goto_prev)
 
 
-    def cmdGotoPrev(self): # self is required as an argument      
-      printDirection("previous")
-        
-    def cmdGotoNext(self):
-      printDirection("next")
+    def cmd_goto_prev(self): # self is required as an argument
+        "actions when the previous button is pressed"
+        print_direction("previous")
+
+
+    def cmd_goto_next(self):
+        "actions when the next button is pressed"
+        print_direction("next")
 
 # global
-def printDirection(direction):
-  print ("skipping to " + direction + " song ")    
+def print_direction(direction):
+    "outputs on the console"
+    print ("skipping to " + direction + " song ")
 
 if __name__ == '__main__':
     # Create the Qt Application
