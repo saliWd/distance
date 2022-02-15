1b5f5de53fb24c663591c3880d045e47e45edffe
saliWd <57621907+saliWd@users.noreply.github.com>
2022-02-15 16:00:06 +0100

    new start with different app. Working

--- androidApp/app/src/main/java/ch/widmedia/swimmeter/utils/extensionFunctions/Views.kt
+++
@@ -1,9 +0,0 @@
-package ch.widmedia.swimmeter.utils.extensionFunctions
-
-import android.util.TypedValue
-import android.view.View
-
-fun View.addRipple() = with(TypedValue()) {
-    context.theme.resolveAttribute(android.R.attr.selectableItemBackground, this, true)
-    setBackgroundResource(resourceId)
-}
