1fce3aa37f44f1a134aa4c1cee92ecc7d6a948e0
saliWd <57621907+saliWd@users.noreply.github.com>
2023-04-11 10:25:39 +0200

    currently working nicely on Mi10

---
+++ swimmeterApp/app/src/main/java/ch/widmedia/swimmeter/blescanner/model/BleDevice.kt
@@ -0,0 +1,12 @@
+package ch.widmedia.swimmeter.blescanner.model
+
+/**
+ * A class that represents a BLE device.
+ */
+data class BleDevice(val name: String) {
+    companion object {
+        fun createBleDevicesList(): MutableList<BleDevice> {
+            return mutableListOf()
+        }
+    }
+}
