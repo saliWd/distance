1c7cead65734feb12e8f79a50ed30a199487c7b2
saliWd <57621907+saliWd@users.noreply.github.com>
2024-02-09 16:38:57 +0100

    works almost as expected

--- receiver/picow/main.py
+++ receiver/picow/main.py
@@ -1,54 +1,48 @@
+##
+# example taken from https://github.com/micropython/micropython-lib/blob/master/micropython/bluetooth/aioble/examples/temp_client.py 
+# (and then changed a lot)
+##
+
 import sys
 
 sys.path.append("")
 
-from micropython import const
-
 import uasyncio as asyncio
 import aioble
-import bluetooth
 
-import random
-import struct
-
-async def find_temp_sensor():
+async def find_beacon():
     # Scan for 5 seconds, in active mode, with very low interval/window (to maximise detection rate).
     async with aioble.scan(5000, interval_us=30000, window_us=30000, active=True) as scanner:
         async for result in scanner:
             if(result.name()): # most are empty...
                 if result.name()[0:11] == "widmedia.ch":
-                    return result.device
+                    return result
                 else:
                     print("did find something but name does not match. Name is: "+result.name())
     return None
 
 
 async def main():
-    device = await find_temp_sensor()
+    result = await find_beacon()
+    device = result.device
     if not device:
         print("beacon not found")
         return
 
     try:
         print("Connecting to", device)
+        print("connection try name: "+result.name())
         connection = await device.connect()
     except asyncio.TimeoutError:
         print("Timeout during connection")
         return
 
-
-#     async with connection:
-#        try:
-#            temp_service = await connection.service(_ENV_SENSE_UUID)
-#            temp_characteristic = await temp_service.characteristic(_ENV_SENSE_TEMP_UUID)
-#        except asyncio.TimeoutError:
-#            print("Timeout discovering services/characteristics")
-#            return
-
-#        while True:
-#            temp_deg_c = _decode_temperature(await temp_characteristic.read())
-#            print("Temperature: {:.2f}".format(temp_deg_c))
-#            await asyncio.sleep_ms(1000)
+    async with connection:
+        while True:
+            print("reached the while-true loop")
+            print("Name: "+result.name())
+            print("RSSI: "+str(result.rssi)) # no error but value is always -64... 
+            await asyncio.sleep_ms(1000)
 
 
 asyncio.run(main())
