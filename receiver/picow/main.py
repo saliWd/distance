87808bff22874160dfe015a3d73292547af1f64c
fqw8852 <daniel.widmer@varian.com>
2024-08-09 14:04:05 +0200

    re-organizing files, adding a function file

--- receiver/picow/main.py
+++ receiver/picow/main.py
@@ -13,14 +13,11 @@ from math import floor
 from micropython import const #type: ignore
 
 import uasyncio as asyncio # type: ignore (this is a pylance ignore warning directive)
-import aioble # type: ignore (this is a pylance ignore warning directive)
 
 # files on file system
-from lcd import LCD_disp # import the display class
 from BEACON_SIM import BEACON_SIM # import the simulator class
 
-import my_config
-CONFIG = my_config.get_config() # which device address do I look for and debug stuff like beacon simulation variables
+from my_functions import LCD, CONFIG, load_background, find_beacon
 
 beaconSim = BEACON_SIM(CONFIG)
 
@@ -34,19 +31,7 @@ MAX_NUM_HIST = const(35)    # [num of entries] corresponds to 240 seconds, max d
 
 ## global variables
 f_dataLog = open('logData.csv', 'a') # append
-# LCD calls the framebuf module, see https://docs.micropython.org/en/latest/library/framebuf.html
-LCD = LCD_disp() # 240px high, 320px wide, see https://www.waveshare.com/wiki/Pico-ResTouch-LCD-2.8
 LOOP_MAX = const(20000) # 20k corresponds to at least 2.2h (with 0.4 secs per meas)
-
-async def find_beacon():
-    # async with aioble.scan(duration_ms=5000) as scanner: # scan for 5s in passive mode. NB: active mode may generete memAlloc issues
-    async with aioble.scan(5000, interval_us=30000, window_us=30000, active=True) as scanner: # Scan for 5 seconds, in active mode, with very low interval/window (to maximise detection rate).
-        async for result in scanner:
-            if result.name() and result.name()[0:11] == CONFIG['beacon_name']: # most are empty...
-                addr = "%s" % result.device # need to get string representation first
-                if addr[32:37] == CONFIG['mac_addr_short']: # last 5 characters of MAC_ADDR
-                    return result
-    return None
 
 def print_lcd_dbg(meas:list, laneCounter:int):
     X = const(10)
@@ -197,21 +182,6 @@ def draw_segment(x:int, y:int, horiz:bool):
         coord = array.array('I', [4,0, 0,4, 0,52, 4,56, 8,52, 8,4])
     LCD.poly(x, y, coord, LCD.WHITE, True) # array is required, can't write the coordinates directly
     return # NB: no lcd.show_up as this is called after all segments are drawn
-
-def load_background():
-    LCD.bl_ctrl(100)
-    LCD.fill(LCD.BLACK)
-    LCD.show_up() # the code below takes some time, have it black from start onwards
-    
-    BG_IMAGE_SIZE_BYTE = const(60*320*2) # I don't load the full image, it's too big/slow. Only part of it and the rest is constant color
-    BUF_SIZE = const(256) # make sure the image size and the buffer size are nicely arranged
-    with open ('background.bin', "rb") as file:
-        for bufPos in range(0, BG_IMAGE_SIZE_BYTE, BUF_SIZE):
-            buffer = array.array('b', file.read(BUF_SIZE)) # file read command itself is taking long
-            for arrPos in range(0, BUF_SIZE):
-                LCD.buffer[bufPos+arrPos] = buffer[arrPos]
-    file.close()
-    LCD.show_up()
 
 # main program
 async def main():
